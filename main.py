38163907fa544b50520a100a492951f8f27c4187
vahid <vahid91630@gmail.com>
2025-07-08 00:20:27 +0330

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-import os
+
+import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = os.getenv("TELEGRAM_TOKEN")
-ADMIN_ID = 6160499507
+TOKEN = "7847661218:AAEIHUcwg2gb7jF8zdK75w2Xk_exIewWAPU"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if update.effective_user.id != ADMIN_ID:
+    if update.effective_user.username != "vahid91640":
         return
-    await update.message.reply_text("Bot is running...")
+    await update.message.reply_text("سلام مدیر عزیز! ربات در خدمت شماست.")
 
 if __name__ == "__main__":
     app = ApplicationBuilder().token(TOKEN).build()
