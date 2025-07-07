911776cd50a8e398aa00c5ed7b7832c46e9c148a
vahid <vahid91630@gmail.com>
2025-07-07 20:22:59 +0330

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello from Batis CRM!'
+    return 'سلام از Kodex!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
