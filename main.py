d1622a89142846ed832ea57d51e24a98e7e0d054
vahid <vahid91630@gmail.com>
2025-07-07 20:43:11 +0330

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ import os
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 bot = telebot.TeleBot(BOT_TOKEN)
-
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return 'روبات Kodex آماده‌ست!'
 
 @app.route(f'/{BOT_TOKEN}', methods=['POST'])
@@ -21,9 +20,8 @@ def webhook():
 def handle_start(message):
     bot.send_message(message.chat.id, "سلام! من ربات CRM باتیس هستم. پیام‌هات رو بفرست.")
 
-# سایر message_handler ها رو بعداً اضافه می‌کنیم
-
 if __name__ == '__main__':
+    webhook_url = f"https://codex-4zmj.onrender.com/{BOT_TOKEN}"
     bot.remove_webhook()
-    bot.set_webhook(url=f"https://codex-4zmj.onrender.com/{BOT_TOKEN}")
+    bot.set_webhook(url=webhook_url)
     app.run(host='0.0.0.0', port=10000)
