f44cc59117716b22e3c8b14701e49a915e718545
vahid <vahid91630@gmail.com>
2025-07-07 20:19:32 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from flask import Flask, request
-import os
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/webhook", methods=["POST"])
-def webhook():
-    return "Webhook received", 200
+@app.route('/')
+def home():
+    return 'Hello from Batis CRM!'
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
